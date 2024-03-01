2b1005ccde3c53bcdbe4d2f4aec694bd1a4b8526
DylBrad <dylanbradley1@hotmail.com>
2024-03-01 23:41:42 +0000

    Style consent form

--- src/app/components/Consent.js
+++ src/app/components/Consent.js
@@ -1,22 +1,31 @@
+import FormHeader from './form/FormHeader';
+
 const Consent = () => {
   return (
     <>
-      <h2>Consent</h2>
+      <FormHeader>Consent</FormHeader>
       <hr className="divider"></hr>
 
-      <input type="checkbox"></input>
-      <p>
-        Please confirm that you understand the <span>Appointment Proccess</span>
-      </p>
-      <input type="checkbox"></input>
-      <p>
-        By submitting to this form, you are agreeing to our{' '}
-        <span>Privacy Policy</span>
-      </p>
-      <input type="checkbox"></input>
-      <p>Consent to sharing your data with VHI healthcare</p>
+      <div className="my-2 flex text-[11px]">
+        <input type="checkbox" className="mr-2"></input>
+        <p>
+          Please confirm that you understand the{' '}
+          <span className="text-cyan-400 underline">Appointment Proccess</span>
+        </p>
+      </div>
+      <div className="my-2 flex text-[11px]">
+        <input type="checkbox" className="mr-2"></input>
+        <p>
+          By submitting to this form, you are agreeing to our{' '}
+          <span className="text-cyan-400 underline">Privacy Policy</span>
+        </p>
+      </div>
+      <div className="my-2 flex text-[11px]">
+        <input type="checkbox" className="mr-2"></input>
+        <p>Consent to sharing your data with VHI healthcare</p>
+      </div>
 
-      <p>
+      <p className="text-[11px]">
         AllView Healthcare and VHI Healthcare are two separate entities involved
         in the provision of this healthcare service. As part of your medical
         treatment, it may be necessary to share you personal and medical
